7cd0d6dc4725848549676c84bf776e81ce62aac1
Daniel Morrison <iLogical8502@gmail.com>
2026-02-18 12:17:14 +0000

    Refactor folder color system to use `FolderColorEntry` resource for improved flexibility

---
+++ addons/fancy_folders/folder_color_entry.gd
@@ -0,0 +1,12 @@
+﻿extends Resource
+class_name FolderColorEntry
+
+# This exported enum creates the dropdown automatically
+enum FolderColor { RED, ORANGE, YELLOW, GREEN, TEAL, BLUE, PURPLE, PINK, GRAY }
+
+@export var folder_name: String = ""
+@export var color: FolderColor = FolderColor.GRAY
+
+# Helper to get the lowercase string Godot needs
+func get_color_string() -> String:
+	return FolderColor.keys()[color].to_lower()
