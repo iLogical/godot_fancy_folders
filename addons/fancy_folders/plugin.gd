e095a131f733b3387840cae9104105b597e731bd
Daniel Morrison <iLogical8502@gmail.com>
2026-02-18 12:25:50 +0000

    Revert "Refactor folder color system to use `FolderColorEntry` resource for improved flexibility"
    This reverts commit 7cd0d6dc4725848549676c84bf776e81ce62aac1.

--- addons/fancy_folders/plugin.gd
+++ addons/fancy_folders/plugin.gd
@@ -1,46 +1,62 @@
 @tool
 extends EditorPlugin
 
-const SETTINGS_KEY: String = "folder_autocolor/config/rules"
+const SETTINGS_KEY: String = "folder_autocolor/config/targets"
 const COLOR_DICT_PATH: String = "file_customization/folder_colors"
 
+# Default fallback targets
+var default_targets: Dictionary[String, String] = {
+	"scripts": "purple",
+	"scenes": "blue",
+	"assets": "red",
+	"components": "orange",
+	"materials": "yellow",
+	"resources": "green",
+	"shaders": "pink",
+	"test": "teal",
+	"addons": "gray"
+}
 
 func _enter_tree() -> void:
+	# 1. Register the setting if it doesn't exist
 	if not ProjectSettings.has_setting(SETTINGS_KEY):
+		ProjectSettings.set_setting(SETTINGS_KEY, default_targets)
 	
-		# Get default folder colours
-		var defaults: Array[FolderColorEntry] = _get_defaults()
-		
-		# Initialize with an empty array of our custom resource type
-		ProjectSettings.set_setting(SETTINGS_KEY, defaults)
-	
-	# Tell the editor this is an Array of FolderColorEntry resources
+	# 2. Add property info so it shows up nicely in the UI
 	var info: Dictionary[String, Variant] = {
 		"name": SETTINGS_KEY,
-		"type": TYPE_ARRAY,
-		"hint": PROPERTY_HINT_TYPE_STRING,
-		"hint_string": "24/17:FolderColorEntry" # Magic string for Array[Resource]
+		"type": TYPE_DICTIONARY,
+		"hint": PROPERTY_HINT_NONE,
+		"hint_string": "Folder Name (Key) to Color Name (Value)"
 	}
 	ProjectSettings.add_property_info(info)
+	ProjectSettings.set_initial_value(SETTINGS_KEY, default_targets)
 	
-	get_editor_interface().get_resource_filesystem().filesystem_changed.connect(_update_folder_colors)
+	# 3. Connect filesystem signals
+	var fs: EditorFileSystem = get_editor_interface().get_resource_filesystem()
+	fs.filesystem_changed.connect(_update_folder_colors)
+	_update_folder_colors()
+
+func _exit_tree() -> void:
+	var fs: EditorFileSystem = get_editor_interface().get_resource_filesystem()
+	if fs.filesystem_changed.is_connected(_update_folder_colors):
+		fs.filesystem_changed.disconnect(_update_folder_colors)
 
 func _update_folder_colors() -> void:
-	var rules = ProjectSettings.get_setting(SETTINGS_KEY, [])
+	# Always pull the latest targets from Project Settings
+	var targets = ProjectSettings.get_setting(SETTINGS_KEY, default_targets)
 	var current_colors = ProjectSettings.get_setting(COLOR_DICT_PATH, {})
-	var changed: bool = false
 	
 	var folders: Array[String] = _get_all_folders("res://")
+	var changed: bool = false
 	
 	for path in folders:
 		var folder_name: String = path.get_base_dir().get_file().to_lower()
-		
-		for rule in rules:
-			if rule is FolderColorEntry and rule.folder_name.to_lower() == folder_name:
-				var target_color: String = rule.get_color_string()
-				if current_colors.get(path) != target_color:
-					current_colors[path] = target_color
-					changed = true
+		if targets.has(folder_name):
+			var color = targets[folder_name]
+			if current_colors.get(path) != color:
+				current_colors[path] = color
+				changed = true
 	
 	if changed:
 		ProjectSettings.set_setting(COLOR_DICT_PATH, current_colors)
@@ -59,26 +75,3 @@ func _get_all_folders(path: String) -> Array[String]:
 				results.append_array(_get_all_folders(full_path))
 			file_name = dir.get_next()
 	return results
-	
-	
-func _get_defaults() -> Array[FolderColorEntry]:
-		var defaults:Array[FolderColorEntry] = []
-		
-		defaults.append(_add_default.call("addons", FolderColorEntry.FolderColor.GRAY))
-		defaults.append(_add_default.call("scenes", FolderColorEntry.FolderColor.BLUE))
-		defaults.append(_add_default.call("assets", FolderColorEntry.FolderColor.RED))
-		defaults.append(_add_default.call("components", FolderColorEntry.FolderColor.ORANGE))
-		defaults.append(_add_default.call("materials", FolderColorEntry.FolderColor.YELLOW))
-		defaults.append(_add_default.call("scripts", FolderColorEntry.FolderColor.PURPLE))
-		defaults.append(_add_default.call("resources", FolderColorEntry.FolderColor.GREEN))
-		defaults.append(_add_default.call("shaders", FolderColorEntry.FolderColor.PINK))
-		defaults.append(_add_default.call("test", FolderColorEntry.FolderColor.TEAL))
-		
-		return defaults
-	
-	
-func _add_default(folder: String, color_idx: int) -> FolderColorEntry:
-		var entry: FolderColorEntry = FolderColorEntry.new()
-		entry.folder_name = folder
-		entry.color = color_idx # Refers to the Enum index in FolderColorEntry
-		return entry
